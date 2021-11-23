7304f8e070b62680aef63c6eb7a540a36ddc8855
alexzhengzzz <839910307@qq.com>
2021-11-23 12:43:21 -0500

    add timer

--- nsspider/main.py
+++ nsspider/main.py
@@ -1,6 +1,15 @@
 import os
+import datetime
+from time import sleep
 
 if __name__ == '__main__':
-    while True:
-        #os.system("scrapy crawl url-spider")
+    starttime = datetime.datetime.now()
+    for _ in range(2):
+        # os.system("scrapy crawl url-spider")
         os.system("scrapy crawl pic-spider")
+    endtime = datetime.datetime.now()
+    duration = endtime - starttime
+    file_num = len(os.listdir("./pic/full"))
+    print("total time: ", duration.seconds)
+    print("file num: ", file_num)
+    print("file per second: ", file_num / duration.seconds)
